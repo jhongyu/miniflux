e60789447edb5e7bc357370d35d7b8d34e87210b
jhongyu <jhy172691619@gmail.com>
2026-04-02 19:45:55 +0800

    fix(migrations): swap priority and ignore_entry_updates columns in feeds table

--- internal/database/migrations.go
+++ internal/database/migrations.go
@@ -1428,11 +1428,11 @@ var migrations = [...]func(tx *sql.Tx) error{
 		return err
 	},
 	func(tx *sql.Tx) (err error) {
-		_, err = tx.Exec(`ALTER TABLE feeds ADD COLUMN ignore_entry_updates bool default 'f'`)
+		_, err = tx.Exec(`ALTER TABLE feeds ADD COLUMN priority int default 0`)
 		return err
 	},
 	func(tx *sql.Tx) (err error) {
-		_, err = tx.Exec(`ALTER TABLE feeds ADD COLUMN priority int default 0`)
+		_, err = tx.Exec(`ALTER TABLE feeds ADD COLUMN ignore_entry_updates bool default 'f'`)
 		return err
 	},
 }
